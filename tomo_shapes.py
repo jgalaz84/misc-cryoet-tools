acd095a8407fb6c0336f136e950a08c1e6b27dec
Jesus G. Galaz-Montoya <jgalaz@gmail.com>
2023-03-27 02:03:52 -0700

    Cleaned up formatting.

--- tomo_shapes.py
+++ tomo_shapes.py
@@ -1,31 +1,7 @@
 #!/usr/bin/env python
 #
 # Author: Jesus Galaz-Montoya 04/2020; last modification: 01/2023
-#
-# This software is issued under a joint BSD/GNU license. You may use the
-# source code in this file under either license. However, note that the
-# complete EMAN2 and SPARX software packages have some GPL dependencies,
-# so you are responsible for compliance with the licenses of these packages
-# if you opt to use BSD licensing. The warranty disclaimer below holds
-# in either instance.
-#
-# This complete copyright notice must be included in any revised version of the
-# source code. Additional authorship citations may be added, but existing
-# author citations must be preserved.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  2111-1307 USA
+
 
 from past.utils import old_div
 from builtins import range
@@ -40,8 +16,13 @@ from EMAN2 import *
 from EMAN2_utils import *
 from EMAN2jsondb import JSTask,jsonclasses
 
+import time
+from datetime import timedelta
+
 
 def main():
+	start = time.perf_counter()
+
 	progname = os.path.basename(sys.argv[0])
 	usage = """prog [options]
 	This programs can generate 3D shapes and multiple copies of 3D shapes (optionally layered and softened) at different scales and in different orientations, 
@@ -367,6 +348,10 @@ def main():
 		f.writelines(lines)
 
 	E2end(logger)
+
+	elapsed = time.perf_counter() - start	
+	print(str(timedelta(seconds=elapsed)))
+	
 	return
 
 
